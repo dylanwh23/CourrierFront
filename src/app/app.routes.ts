83b47fa1ef641402c96aceadb976f4ef3af3ac73
Dylan <dylancamblong@gmail.com>
2025-05-28 19:44:36 -0300

    hola

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TarifasComponent } from './tarifas/tarifas.component';
+import { SoporteComponent } from './soporte/soporte.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'tarifas', component: TarifasComponent },
+  { path: 'soporte', component: SoporteComponent },
+  { path: 'calculadora', component: CalculadoraComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent }
+];
