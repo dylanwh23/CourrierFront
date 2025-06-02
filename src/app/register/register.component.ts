c1564b806e7a481da2b6c8819a0f8dd9c8d578e7
Dylan <dylancamblong@gmail.com>
2025-06-02 02:50:57 -0300

    80% login

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { UsuarioService } from '../servicios/usuario.service';
 import { NgFor, NgIf } from '@angular/common';
 import { CommonModule } from '@angular/common';
@@ -9,12 +14,13 @@ import { CommonModule } from '@angular/common';
   selector: 'app-register',
   imports: [RouterLink, ReactiveFormsModule, NgFor, CommonModule, NgIf],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent implements OnInit {
   userForm!: FormGroup;
   mensaje: string = '';
   errores: { [key: string]: string[] } = {};
+  usuarioCreado: boolean = false;
   constructor(
     private fb: FormBuilder,
     private usuarioService: UsuarioService
@@ -29,7 +35,7 @@ export class RegisterComponent implements OnInit {
       confirm_password: ['', [Validators.required, Validators.minLength(8)]],
       confirm_email: ['', [Validators.required, Validators.email]],
       date_of_birth: ['', [Validators.required]],
-      cedula: ['', [Validators.required]]
+      cedula: ['', [Validators.required]],
     });
   }
   onSubmit() {
@@ -45,26 +51,22 @@ export class RegisterComponent implements OnInit {
         return;
       }
 
-  
-
-         this.usuarioService.altaUsuario(formData).subscribe({
-      next: (res) => {
-        this.mensaje = 'Usuario registrado correctamente';
-        console.log(res);
-      },
-      error: (err) => {
-        if (err.status === 422) {
-      this.mensaje = 'Errores de validación:';
-      this.errores = err.error.errors; // Esto es un objeto con los errores por campo
-    } else {
-      this.mensaje = 'Error inesperado en el registro';
-    }
-      }
-    });
+      this.usuarioService.altaUsuario(formData).subscribe({
+        next: (res) => {
+          this.mensaje = 'Usuario registrado correctamente';
+          console.log(res);
+          this.usuarioCreado = true;
+        },
+        error: (err) => {
+          if (err.status === 422) {
+            this.errores = err.error.errors; // Esto es un objeto con los errores por campo
+          } else {
+            this.mensaje = 'Error inesperado en el registro';
+          }
+        },
+      });
     } else {
       console.log('Formulario inválido');
     }
   }
-  
-  
 }
