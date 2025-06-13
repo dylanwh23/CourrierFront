f9ceaeb04c04c46d9ca3527b878dbe377629d55b
Dylan <dylancamblong@gmail.com>
2025-06-12 23:18:16 -0300

    Refactor profile section and enhance order/reclamo components with structured HTML and styling

--- src/app/profile/profileSecciones/mis-ordenes/mis-ordenes.component.ts
+++ src/app/profile/profileSecciones/mis-ordenes/mis-ordenes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-mis-ordenes',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './mis-ordenes.component.css'
 })
 export class MisOrdenesComponent {
+  @HostBinding('class') classes = 'flex flex-col min-h-5/6 min-w-5/6 bg-white mx-auto p-4 rounded-lg shadow-lg'
 
 }
--- src/app/profile/profileSecciones/mis-reclamos/mis-reclamos.component.ts
+++ src/app/profile/profileSecciones/mis-reclamos/mis-reclamos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-mis-reclamos',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './mis-reclamos.component.css'
 })
 export class MisReclamosComponent {
+  @HostBinding('class') classes = 'flex flex-col min-h-5/6 min-w-5/6 bg-white mx-auto p-4 rounded-lg shadow-lg'
 
 }
